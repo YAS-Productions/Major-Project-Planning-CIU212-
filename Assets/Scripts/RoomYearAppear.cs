015d80cc2c532212833feb5bf90785bee3a1fdfd
ramkillnani <ramkhilnani94@gmail.com>
2021-12-15 01:03:11 +1100

    added year on room start and boom

---
+++ Assets/Scripts/RoomYearAppear.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomYearAppear : MonoBehaviour
+{
+    public GameObject canvas;
+    private AudioSource audioSource;
+    public AudioClip boom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = FindObjectOfType<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TriggerYear()
+    {
+        canvas.SetActive(true);
+        audioSource.PlayOneShot(boom);
+        Destroy(canvas, 2);
+    }
+}
