abc0209c1d0c03aeb9f102d2ba889309909d99f6
Chibi-Burrito <70257495+Chibiburr@users.noreply.github.com>
2022-01-08 16:22:22 +1100

    Start on Suburban House Kitchen

---
+++ Assets/Eirene/American Retro Cafe/Readme.txt.txt
@@ -0,0 +1,14 @@
+Thank you for your purchase!
+
+To achieve the same graphics as you can see on screenshots and video you have to do next steps:
+
+1. Switch the Color Space of the project to Linear in Edit > Project Settings > Player > Other Settings;
+
+2. Use Deferred rendering path. You can chose it in Edit > Project Settings > Graphics;
+
+3. Use free Unity asset "Post Processing Stack v2" and the profile for it, which you can find in the Eirene/American Retro Cafe/Demo Scene folder.
+
+
+To use Substance files in Unity 2017, simply unzip the Substance.zip archive that is placed in the Eirene/American Retro Cafe/ folder. If you use Unity 2018+, you have to download and import a free asset called "Substance in Unity" before unzipping the archive to avoide errors.
+
+Thanks!
---
+++ Assets/Eirene/American Retro Cafe/Scripts/RC_DoorScript.cs
@@ -0,0 +1,70 @@
+﻿namespace RetroCaffe
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class RC_DoorScript : MonoBehaviour
+    {
+
+        public string playerTag = "Player";
+        private bool inTrigger = false;
+        private Animation doorAnim;
+        public AnimationClip anim;
+        private bool isOpen = false;
+
+        // Use this for initialization
+        void Start()
+        {
+            if (GetComponent<Animation>() != null)
+            {
+                doorAnim = GetComponent<Animation>();
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (inTrigger)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (isOpen)
+                    {
+                        OpenCloseDoor(1, -1);
+                        isOpen = false;
+                    }
+                    else
+                    {
+                        OpenCloseDoor(0, 1);
+                        isOpen = true;
+                    }
+
+
+                }
+            }
+        }
+
+        public void OpenCloseDoor(float _time, float _speed)
+        {
+            doorAnim[anim.name].normalizedTime = _time;
+            doorAnim[anim.name].speed = _speed;
+            doorAnim.Play();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == playerTag)
+            {
+                inTrigger = true;
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.tag == playerTag)
+            {
+                inTrigger = false;
+            }
+        }
+    }
+}
