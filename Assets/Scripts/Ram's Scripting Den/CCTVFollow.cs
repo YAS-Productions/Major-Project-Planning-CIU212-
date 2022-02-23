f0cfa6a6325b631517aba0a72958a1be0df4d6ea
ramkillnani <ramkhilnani94@gmail.com>
2022-02-23 12:33:16 +1100

    added cam look at delay
    x

--- Assets/Scripts/Ram's Scripting Den/CCTVFollow.cs
+++ Assets/Scripts/Ram's Scripting Den/CCTVFollow.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class CCTVFollow : MonoBehaviour
 {
     public Transform target;
+    public float delay = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
+        //transform.LookAt(target);
     }
+
+    private void FixedUpdate()
+    {
+        var rotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * delay);
+
+    }
+
+    
 }
