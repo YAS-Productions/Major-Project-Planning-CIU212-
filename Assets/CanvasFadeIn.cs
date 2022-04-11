d7169282e7915e5abf8ba5afc93c4696c361e23a
ramkillnani <ramkhilnani94@gmail.com>
2022-04-11 10:07:46 +1000

    KF: Cutscene Linked
    x

---
+++ Assets/CanvasFadeIn.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasFadeIn : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            FadeOut();
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            FadeIn();
+        }
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(StartFading());
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(StartUnFading());
+    }
+
+    IEnumerator StartFading()
+    {
+       
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= Time.deltaTime / 2;
+            yield return null;
+        }
+        canvasGroup.interactable = false;
+        yield return null;
+
+    }
+    IEnumerator StartUnFading()
+    {
+        //canvasGroup = GetComponent<CanvasGroup>();
+        while (canvasGroup.alpha != 1)
+        {
+            canvasGroup.alpha += Time.deltaTime * 1;
+            yield return null;
+        }
+
+
+        canvasGroup.interactable = true;
+        yield return null;
+
+    }
+
+    public void StartPlayer()
+    {
+        player.SetActive(true);
+    }
+}
