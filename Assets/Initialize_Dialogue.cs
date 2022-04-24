e46f7b1501650f943a3b2ecd02814a77e52391e0
ramkillnani <ramkhilnani94@gmail.com>
2022-04-24 22:09:22 +1000

    Audio: KF, Corriander, Detecto
    x

---
+++ Assets/Initialize_Dialogue.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initialize_Dialogue : MonoBehaviour
+{
+    public GameObject KF_Dialogue_Welcome;
+
+    public GameObject dialogue;
+
+    private void Start()
+    {
+        Invoke("PlayDialogue", 6.6f);
+    }
+
+    #region KF
+    void PlayDialogue()
+    {
+        KF_Dialogue_Welcome.SetActive(true);
+    }
+
+    #endregion
+
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            dialogue.SetActive(true);
+        }
+    }
+
+   
+}
